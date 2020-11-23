1c9c6ff81a05fb75528e840fd9af87225973c02e
Laurent Curau <laurent@thecodeshed.dev>
2020-11-23 01:42:17 +0100

    Scraper of INSEE indexes

---
+++ lib/insee_scraper.rb
@@ -0,0 +1,6 @@
+require "insee_scraper/version"
+
+module InseeScraper
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/insee_scraper/version.rb
@@ -0,0 +1,3 @@
+module InseeScraper
+  VERSION = "0.1.0"
+end
